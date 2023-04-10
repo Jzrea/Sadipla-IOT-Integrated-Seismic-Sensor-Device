69709989c8e6b5d01220225792832a53f62a1a25
Jzrea <111337870+Jzrea@users.noreply.github.com>
2023-04-10 08:43:00 +0800

    1

--- server.js
+++ server.js
@@ -28,6 +28,7 @@ app.io = require('socket.io')({
       'http://localhost:4000',
     ],
   },
+  transports: ['websocket'],
 });
 const socketIORouter = require('./routes/socketIO.routes')(app.io);
 const quakesRouter = require('./routes/quakes.routes');
